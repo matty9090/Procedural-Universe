c348f238eed96b1fb5b6ca20d2d6a24ac99575eb
Matthew Lowe <lowe.matthew@mail.com>
2019-10-18 20:04:28 +0100

    Galaxy seeder

--- src/Sim/GalaxySeeder.cpp
+++ src/Sim/GalaxySeeder.cpp
@@ -1,53 +1,111 @@
 #include "GalaxySeeder.hpp"
-
-#include <random>
 #include <DirectXColors.h>
 
-GalaxySeeder::GalaxySeeder(std::vector<Particle>& particles) : Particles(particles)
+using DirectX::SimpleMath::Vector3;
+using DirectX::SimpleMath::Color;
+
+GalaxySeeder::GalaxySeeder(std::vector<Particle>& particles)
+    : Particles(particles),
+      DistZ(0.0f, 16.0f),
+      DistCol(0.2f, 1.0f),
+      DistRed(0.0f, 0.3f),
+      DistMass(1e28, 1e30)
 {
 
 }
 
 void GalaxySeeder::Seed()
 {
-    std::default_random_engine generator;
-    std::uniform_real_distribution<double> dist(-320.0f, 320.0);
-    std::uniform_real_distribution<double> dist_vel(0.0f, 3.0f);
-    std::uniform_real_distribution<double> dist_mass(1e28, 1e30);
+    LocalNum = 0;
 
-    unsigned int i = 0;
+    std::uniform_real_distribution<float> dist_pos(-1000.0f, 1000.0f);
+    std::uniform_real_distribution<double> dist_vel(0.8, 1.2);
 
-    for(i; i < Particles.size() / 2; ++i)
+    CreateSpiralArm(0.0f, ArmPDist / 2);
+    CreateSpiralArm(3.14f, ArmPDist / 2);
+
+    AddParticle(Centre, Vec3d(), 1e32);
+
+    for(int i = 0; i < Particles.size() - LocalNum; ++i)
     {
-        Particles[i].Position.x = static_cast<float>(dist(generator)) - 600.0f;
-        Particles[i].Position.y = static_cast<float>(dist(generator));
-        Particles[i].Position.z = static_cast<float>(dist(generator));
-        Particles[i].Velocity.x = static_cast<float>(dist_vel(generator));
-        Particles[i].Velocity.y = static_cast<float>(0.0);
-        Particles[i].Velocity.z = static_cast<float>(0.0);
-        Particles[i].Mass   = dist_mass(generator);
-        Particles[i].Forces = Vec3d();
-        Particles[i].Colour = DirectX::Colors::White;
-        Particles[i].OriginalColour = Particles[i].Colour;
+        Vector3 pos;
+        pos.x = static_cast<float>(dist_pos(Gen));
+        pos.y = static_cast<float>(dist_pos(Gen));
+        pos.z = static_cast<float>(DistZ(Gen));
+
+        if (Vector3::Distance(pos, Centre) > 720.0f)
+        {
+            --i;
+            continue;
+        }
+
+        Vector3 norm = pos - Centre;
+        Vector3 tangent = norm.Cross(Vector3(0.0f, 0.0f, 1.0f));
+
+        Vec3d vel(tangent.x, tangent.y, tangent.z);
+        vel *= dist_vel(Gen) * 1e14;
+
+        if (!AddParticle(pos, vel, DistMass(Gen)))
+            break;
+    }
+}
+
+bool GalaxySeeder::AddParticle(
+    DirectX::SimpleMath::Vector3 Pos,
+    Vec3<double> Vel,
+    double Mass
+)
+{
+    if(LocalNum < Particles.size())
+    {
+        Particles[LocalNum].Position = Pos;
+        Particles[LocalNum].Velocity = Vel;
+        Particles[LocalNum].Mass = Mass;
+        Particles[LocalNum].Colour = Color(DistRed(Gen), DistCol(Gen), DistCol(Gen));
+        Particles[LocalNum].OriginalColour = Particles[LocalNum].Colour;
+        Particles[LocalNum].Forces = Vec3d();
+
+        ++LocalNum;
+
+        return true;
     }
 
-    for(i; i < Particles.size(); ++i)
+    return false;
+}
+
+void GalaxySeeder::CreateSpiralArm(float offset, float dist)
+{
+    std::normal_distribution<float> distx(0.5f, 0.2f);
+    std::uniform_real_distribution<float> disty(0.5f, 0.2f);
+
+    float maxAngle = 6.0f;
+    int loops = floor(maxAngle / 0.1f);
+    float numPerLoop = floor((static_cast<float>(Particles.size()) * dist) / loops);
+
+    for(float angle = 0.0f, r = 2.0f; angle < 6.0f; angle += 0.1f, r += 7.2f)
     {
-        Particles[i].Position.x = static_cast<float>(dist(generator)) + 600.0f;
-        Particles[i].Position.y = static_cast<float>(dist(generator));
-        Particles[i].Position.z = static_cast<float>(dist(generator));
-        Particles[i].Velocity.x = static_cast<float>(dist_vel(generator));
-        Particles[i].Velocity.y = static_cast<float>(dist_vel(generator));
-        Particles[i].Velocity.z = static_cast<float>(dist_vel(generator));
-        Particles[i].Mass   = dist_mass(generator);
-        Particles[i].Forces = Vec3d();
-        Particles[i].Colour = DirectX::Colors::White;
-        Particles[i].OriginalColour = Particles[i].Colour;
+        auto spiral = Vector3(cosf(angle + offset) * r, sinf(angle + offset) * r, 0.0f);
+        auto spiraln = Vector3(cosf(angle + offset + 0.1f) * (r + 10.0f), sinf(angle + offset + 0.1f) * (r + 10.0f), 0.0f);
+        
+        auto normal = (spiral - spiraln);
+        auto mag = normal.Length();
+        normal.Normalize();
+
+        Vector3 tangent = normal.Cross(Vector3(0.0f, 0.0f, 1.0f));
+        tangent.Normalize();
+
+        auto sx = spiral - tangent * 140.0f;
+        auto ex = spiral + tangent * 140.0f;
+        auto sy = spiral - normal  * 100.0f;
+        auto ey = spiral + normal  * 100.0f;
+        
+        for (int i = 0; i < numPerLoop; ++i)
+        {
+            auto position = Vector3::Lerp(sx, ex, distx(Gen)) + Vector3::Lerp(sy, ey, disty(Gen));
+            auto velocity = normal * 2e16 * (1000.0f / mag);
+            position.z = static_cast<float>(DistZ(Gen));
+
+            AddParticle(position, Vec3d(velocity.x, velocity.y, velocity.z));
+        }
     }
-
-    Particles[0].Mass = 1e35;
-    Particles[0].Position = { -300, 0, 0 };
-
-    Particles[Particles.size() / 2].Mass = 1e35;
-    Particles[Particles.size() / 2].Position = { 300, 0, 0 };
 }
--- src/Sim/GalaxySeeder.hpp
+++ src/Sim/GalaxySeeder.hpp
@@ -2,6 +2,9 @@
 
 #include "IParticleSeeder.hpp"
 
+#include <SimpleMath.h>
+#include <random>
+
 class GalaxySeeder : public IParticleSeeder
 {
     public:
@@ -11,4 +14,23 @@ class GalaxySeeder : public IParticleSeeder
 
     private:
         std::vector<Particle>& Particles;
+        int LocalNum = 0;
+
+        bool AddParticle(
+            DirectX::SimpleMath::Vector3 Pos,
+            Vec3d Vel = Vec3d(),
+            double Mass = 1e20
+        );
+
+        void CreateSpiralArm(float offset, float dist);
+
+        const float ArmPDist = 0.8f;
+        
+        std::normal_distribution<float> DistZ;
+        std::uniform_real_distribution<float> DistCol;
+        std::uniform_real_distribution<float> DistRed;
+        std::uniform_real_distribution<double> DistMass;
+        
+        std::default_random_engine Gen;
+        DirectX::SimpleMath::Vector3 Centre;
 };
