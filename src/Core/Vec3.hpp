3671e5a49765e595dbd3d3a1c81349a375b33fe3
Matthew Lowe <lowe.matthew@mail.com>
2019-11-09 15:59:38 +0000

    Update Vec3.hpp

--- src/Core/Vec3.hpp
+++ src/Core/Vec3.hpp
@@ -15,7 +15,7 @@ template <class T = float> struct Vec3 {
 	Vec3() : x(0), y(0), z(0) {}
 	Vec3(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
 
-	inline float Distance(Vec3 v) {
+	inline float Distance(Vec3 v) const {
 		T dx = (v.x - x) * (v.x - x);
 		T dy = (v.y - y) * (v.y - y);
 		T dz = (v.z - z) * (v.z - z);
@@ -23,7 +23,7 @@ template <class T = float> struct Vec3 {
 		return sqrtf(dx + dy + dz);
 	}
 
-    inline float DistanceSqr(Vec3 v) {
+    inline float DistanceSqr(Vec3 v) const {
 		T dx = (v.x - x) * (v.x - x);
 		T dy = (v.y - y) * (v.y - y);
 		T dz = (v.z - z) * (v.z - z);
@@ -31,11 +31,11 @@ template <class T = float> struct Vec3 {
 		return dx + dy + dz;
 	}
 
-	inline float Length() {
+	inline float Length() const {
 		return sqrtf(x * x + y * y + z * z);
 	}
 
-	inline Vec3 Normalised() {
+	inline Vec3 Normalised() const {
 		float m = Length();
 		return Vec3(x / m, y / m, z / m);
 	}
